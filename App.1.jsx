4a112fe86ca92614abd57824702d8c3305ac0c66
Gabriel <145726594+gabydian@users.noreply.github.com>
2024-04-26 21:37:28 -0300

    Add files via upload

---
+++ App.1.jsx
@@ -0,0 +1,19 @@
+import {personagens} from "./personagensRE";
+export function App1() {
+    return (
+        <div>
+            <h4>Personagens da família Redfield</h4>
+            <ul>
+                {personagens
+                    .filter(personagem => personagem.nome.includes('Redfield'))
+                    .map(personagem => (
+                        <li key={personagem.nome}>
+                            <img src={personagem.url} alt={personagem.nome} />
+                            <h3>{personagem.nome}</h3>
+                        </li>
+                    ))}
+            </ul>
+        </div>
+    );
+}
+export default App1;
---
+++ personagensRE.js
@@ -0,0 +1,30 @@
+export const personagens = [
+  {
+    nome: 'Leon S. Kennedy',
+    url: 'https://static.wikia.nocookie.net/residentevil/images/1/1d/LeonRE2RemakeRender.png',
+  },
+  {
+    nome: 'Jill Valentine',
+    url: 'https://i.imgur.com/XOR8PHl.png',
+  },
+  {
+    nome: 'Claire Redfield',
+    url: 'https://i.imgur.com/Jg0s8kP.png',
+  },
+  {
+    nome: 'Chris Redfield',
+    url: 'https://i.imgur.com/mQhIyKH.png',
+  },
+  {
+    nome: 'Ada Wong',
+    url: 'https://i.imgur.com/BS2Kfc8.png',
+  },
+  {
+    nome: 'Albert Wesker',
+    url: 'https://static.wikia.nocookie.net/residentevil/images/e/e4/Wesker_Revelations_2_Render.png',
+  },
+  {
+    nome: 'Alex Wesker',
+    url: 'https://static.wikia.nocookie.net/residentevil/images/d/d8/Alex_Wesker.png',
+  }
+]
